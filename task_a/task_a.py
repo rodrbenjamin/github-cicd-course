2a73260966551340f387b86e5c9daa6e30335c69
Benjamin Rodr <rodrbenj@fel.cvut.cz>
2024-12-17 16:40:18 +0100

    task a pipeline

--- task_a/task_a.py
+++ task_a/task_a.py
@@ -1,6 +1,7 @@
 def square_area(a: int) -> int:
-    """Counts an area of of square."""
-    return a*a
+    """Counts an area of square."""
+    return a * a
+
 
 def fib(n: int) -> int:
     """Count a fibonacci number n."""
@@ -8,12 +9,14 @@ def fib(n: int) -> int:
     for _ in range(n):
         print(a, end=" ")
         a, b = b, a + b
-    
+
     return a
+
 
 def test_square_area() -> None:
     assert square_area(5) == 25
     assert square_area(0) == 0
+
 
 def test_fib() -> None:
     assert fib(0) == 0
@@ -22,4 +25,3 @@ def test_fib() -> None:
     assert fib(3) == 2
     assert fib(5) == 5
     assert fib(10) == 55
-    
