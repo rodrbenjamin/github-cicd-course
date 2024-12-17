475095045a88008679a2111a23b489f8e85a2f0b
Benjamin Rodr <rodrbenj@fel.cvut.cz>
2024-12-17 16:55:30 +0100

    failed test

--- task_a/task_a.py
+++ task_a/task_a.py
@@ -20,7 +20,7 @@ def test_square_area() -> None:
 
 def test_fib() -> None:
     assert fib(0) == 0
-    assert fib(1) == 1
+    assert fib(1) == 3
     assert fib(2) == 1
     assert fib(3) == 2
     assert fib(5) == 5
